51fe9e168b6854ec0adb3a899021f299fd32a273
iHealthwebmaster <webmaster@ihealthlabs.com>
2013-09-26 09:43:50 +0800

    Create GetStarted.aspx.designer.cs

---
+++ Web/GetStarted.aspx.designer.cs
@@ -0,0 +1,10 @@
+namespace Web {
+    
+    
+    public partial class GetStarted {
+        
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        protected global::System.Web.UI.WebControls.Button btnConnect;
+    }
+}
