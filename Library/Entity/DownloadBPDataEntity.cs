d8c395b817fe68452425cac3d90d464137ca083b
iHealthwebmaster <webmaster@ihealthlabs.com>
2013-09-26 09:37:26 +0800

    Create DownloadBPDataEntity.cs

---
+++ Library/Entity/DownloadBPDataEntity.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace iHealthlabs.OpenAPI.Sample.Library
+{
+    [DataContract]
+    public class DownloadBPDataEntity
+    {
+        /// <summary>
+        /// BP level
+        /// </summary>
+        [DataMember]
+        public int BPL { get; set; }
+        /// <summary>
+        /// HP
+        /// </summary>
+        [DataMember]
+        public double HP { get; set; }
+        /// <summary>
+        /// HR
+        /// </summary>
+        [DataMember]
+        public int HR { get; set; }
+        /// <summary>
+        /// IsArr
+        /// </summary>
+        [DataMember]
+        public int IsArr { get; set; }
+        /// <summary>
+        /// LP
+        /// </summary>
+        [DataMember]
+        public double LP { get; set; }
+        /// <summary>
+        /// Lat
+        /// </summary>
+        [DataMember]
+        public double Lat { get; set; }
+        /// <summary>
+        /// Lon
+        /// </summary>
+        [DataMember]
+        public double Lon { get; set; }
+        /// <summary>
+        /// Data ID
+        /// </summary>
+        [DataMember]
+        public string DataID { get; set; }
+        /// <summary>
+        /// Measure date
+        /// </summary>
+        [DataMember]
+        public long MDate { get; set; }
+        /// <summary>
+        /// Last change time
+        /// </summary>
+        [DataMember]
+        public long LastChangeTime { get; set; }
+        /// <summary>
+        /// Note
+        /// </summary>
+        [DataMember]
+        public string Note { get; set; }
+    }
+}
