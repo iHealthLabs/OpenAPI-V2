ca0dcaac029ee454d2463639967dcdea58e661b5
iHealthwebmaster <webmaster@ihealthlabs.com>
2013-09-26 09:40:53 +0800

    Create DownloadWeightDataEntity.cs

---
+++ Library/Entity/DownloadWeightDataEntity.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace iHealthlabs.OpenAPI.Sample.Library
+{
+    [DataContract]
+    public class DownloadWeightDataEntity
+    {
+        /// <summary>
+        /// BMI grade
+        /// </summary>
+        [DataMember]
+        public double BMI { get; set; }
+        /// <summary>
+        /// Bone mineral density 
+        /// </summary>
+        [DataMember]
+        public double BoneValue { get; set; }
+        /// <summary>
+        /// DCI
+        /// </summary>
+        [DataMember]
+        public double DCI { get; set; }
+        /// <summary>
+        /// Body fat 
+        /// </summary>
+        [DataMember]
+        public double FatValue { get; set; }
+        /// <summary>
+        /// Muscle
+        /// </summary>
+        [DataMember]
+        public double MuscaleValue { get; set; }
+        /// <summary>
+        /// Water
+        /// </summary>
+        [DataMember]
+        public double WaterValue { get; set; }
+        /// <summary>
+        /// Weight
+        /// </summary>
+        [DataMember]
+        public double WeightValue { get; set; }
+        /// <summary>
+        /// Data only Serial number
+        /// </summary>
+        [DataMember]
+        public string DataID { get; set; }
+        /// <summary>
+        /// Data measurement of time
+        /// </summary>
+        [DataMember]
+        public long MDate { get; set; }
+        /// <summary>
+        /// Last change time
+        /// </summary>
+        [DataMember]
+        public long LastChangeTime { get; set; }
+        /// <summary>
+        /// Note
+        /// </summary>
+        [DataMember]
+        public string Note { get; set; }
+    }
+}
