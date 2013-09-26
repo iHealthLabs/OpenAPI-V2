f61c4371ecaab5ec02eeacd1fe61e67f411e7adf
iHealthwebmaster <webmaster@ihealthlabs.com>
2013-09-26 09:36:28 +0800

    Create DownloadBGDataResultEntity.cs

---
+++ Library/Entity/DownloadBGDataResultEntity.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace iHealthlabs.OpenAPI.Sample.Library.Entity
+{
+    [DataContract]
+    public class DownloadBGDataResultEntity
+    {
+        public DownloadBGDataResultEntity() { }
+        /// <summary>
+        /// The page data
+        /// </summary>
+        [DataMember]
+        public List<DownloadBGDataEntity> BGDataList { get; set; }
+        /// <summary>
+        /// 2013 5 15 Blood  glucose  units mgdl  mmoll
+        /// </summary>
+        [DataMember]
+        public int BGUnit { get; set; }
+        /// <summary>
+        /// Current Total
+        /// </summary>
+        [DataMember]
+        public int CurrentRecordCount { get; set; }
+        /// <summary>
+        /// Next page URL
+        /// </summary>
+        [DataMember]
+        public string NextPageUrl { get; set; }
+        /// <summary>
+        /// Page number
+        /// </summary>
+        [DataMember]
+        public int PageLength { get; set; }
+        /// <summary>
+        /// Current page
+        /// </summary>
+        [DataMember]
+        public int PageNumber { get; set; }
+        /// <summary>
+        /// Page up URL
+        /// </summary>
+        [DataMember]
+        public string PrevPageUrl { get; set; }
+        /// <summary>
+        /// Total
+        /// </summary>
+        [DataMember]
+        public int RecordCount { get; set; }
+    }
+}
