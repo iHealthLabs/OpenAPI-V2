9ed9e646f138f7dc23b55f4d48929423c6f046d4
iHealthwebmaster <webmaster@ihealthlabs.com>
2013-09-26 09:38:01 +0800

    Create DownloadSleepReportDataEntity.cs

---
+++ Library/Entity/DownloadSleepReportDataEntity.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace iHealthlabs.OpenAPI.Sample.Library.Entity
+{
+    [DataContract]
+    public class DownloadSleepReportDataEntity
+    {
+        /// <summary>
+        /// Sleep efficiency
+        /// </summary>
+        [DataMember]
+        public double SleepEfficiency { get; set; }
+        /// <summary>
+        /// Sleep duration
+        /// </summary>
+        [DataMember]
+        public double FallSleep { get; set; }
+        /// <summary>
+        /// start time
+        /// </summary>
+        [DataMember]
+        public long StartTime { get; set; }
+        /// <summary>
+        /// end time
+        /// </summary>
+        [DataMember]
+        public long EndTime { get; set; }
+        /// <summary>
+        /// note
+        /// </summary>
+        [DataMember]
+        public string Note { get; set; }
+        /// <summary>
+        /// Longitude
+        /// </summary>
+        [DataMember]
+        public double Lon { get; set; }
+        /// <summary>
+        /// Latitude
+        /// </summary>
+        [DataMember]
+        public double Lat { get; set; }
+        /// <summary>
+        /// Data only Serial number
+        /// </summary>
+        [DataMember]
+        public string DataID { get; set; }
+        /// <summary>
+        /// Sleep duration
+        /// </summary>
+        [DataMember]
+        public double HoursSlept { get; set; }
+        /// <summary>
+        /// The length of time awake
+        /// </summary>
+        [DataMember]
+        public double Awake { get; set; }
+
+    }
+}
