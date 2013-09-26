8548d96fb68c6cb640e0fd069ecee2263cd03053
iHealthwebmaster <webmaster@ihealthlabs.com>
2013-09-26 09:43:33 +0800

    Create GetStarted.aspx.cs

---
+++ Web/GetStarted.aspx.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web
+{
+    public partial class GetStarted : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConnect_Click(object sender, EventArgs e)
+        {
+            new iHealthlabs.OpenAPI.Sample.Library.ConnectToiHealthlabs().GetCode();
+        }
+    }
+}
