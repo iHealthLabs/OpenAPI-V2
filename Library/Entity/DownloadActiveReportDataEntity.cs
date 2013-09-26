58b56017c27a02294a86281ea8ec2542fd6c1ce6
iHealthwebmaster <webmaster@ihealthlabs.com>
2013-09-26 09:34:46 +0800

    Create DownloadActiveReportDataEntity.cs

---
+++ Library/Entity/DownloadActiveReportDataEntity.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace iHealthlabs.OpenAPI.Sample.Library.Entity
+{
+    [DataContract]
+    public class DownloadActiveReportDataEntity
+    {
+        /// <summary>
+        /// Steps
+        /// </summary>
+        [DataMember]
+        public double Steps { get; set; }
+        /// <summary>
+        /// Distance
+        /// </summary>
+        [DataMember]
+        public double DistanceTraveled { get; set; }
+        /// <summary>
+        /// Measure time
+        /// </summary>
+        [DataMember]
+        public long MDate { get; set; }
+        /// <summary>
+        /// note
+        /// </summary>
+        [DataMember]
+        public string Note { get; set; }
+        /// <summary>
+        /// Longitude
+        /// </summary>
+        [DataMember]
+        public double Lon { get; set; }
+        /// <summary>
+        /// Latitude
+        /// </summary>
+        [DataMember]
+        public double Lat { get; set; }
+        /// <summary>
+        /// Data only Serial number
+        /// </summary>
+        [DataMember]
+        public string DataID { get; set; }
+        /// <summary>
+        /// Of the total calories consumed
+        /// </summary>
+        [DataMember]
+        public double Calories { get; set; }
+    }
+}
